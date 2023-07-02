c9ec1cb2b4f41fa8166f9ebce1e3e8aa564aa139
Veervikram singh <veervikramsingh132@gmail.com>
2023-07-03 00:16:09 +0530

    minor change

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
 import '@styles/globals.css';
-import Nav from '@components/nav';
+import Nav from '@components/Nav';
 import Provider from '@components/Provider';
 import Footer from '@app/footer/page';
 
